85a81591f3b3e54536775b6ed7c37ee8e1feb58e
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-28 17:03:34 +0200

    sistemata stampa HTML

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 require_once __DIR__ . '/classes/food.php';
 
-$food = new Food('Carne Essiccata', '10€', 'Cani', 'Cibo in scatola', '10/10/2022');
+$food = [
+    new Food('Carne Essiccata', '10€', 'Cani', 'Cibo in scatola', '10/10/2022'),
+    new Food('Semi di girasole', '8€', 'Criceti', 'Cibo in scatola', '01/06/2023'),
+    new Food('Crocchette di salmone', '15€', 'Gatti', 'Cibo in scatola', '01/09/2022'),
+]
 // var_dump($food);die;
 ?>
 <!DOCTYPE html>
@@ -17,9 +21,9 @@ $food = new Food('Carne Essiccata', '10€', 'Cani', 'Cibo in scatola', '10/10/2
     <h1>Animal Planet</h1>
     <h2>Sezione cibo</h2>
     <ul>
-        <?php foreach($food as $food) { ?>
+        <?php foreach($food as $foods) { ?>
             <li>
-                <h3><?php echo $food->name ?></h3>
+                <h3><?php echo $foods->name ?></h3>
             </li>
         <?php } ?>   
     </ul>
